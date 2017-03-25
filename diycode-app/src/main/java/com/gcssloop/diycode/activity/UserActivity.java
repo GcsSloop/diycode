cd49e7acde6b7ab5776509f1b8dda282903c3597
GcsSloop <GcsSloop@gmail.com>
2017-03-26 06:36:28 +0800

    clearUserActivity

--- diycode-app/src/main/java/com/gcssloop/diycode/activity/UserActivity.java
+++ diycode-app/src/main/java/com/gcssloop/diycode/activity/UserActivity.java
@@ -90,7 +90,8 @@ public class UserActivity extends BaseActivity implements View.OnClickListener {
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra(USER);
         if (null != user) {
-            holder.setText(user.getLogin() + "(" + user.getName() + ")", R.id.username);
+            setTitle(user.getLogin());
+            holder.setText(user.getName(), R.id.username);
             holder.loadImage(this, user.getAvatar_url(), R.id.avatar);
             holder.setOnClickListener(this, R.id.follow);
             mDiycode.getUser(user.getLogin());
