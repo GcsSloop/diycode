b44e40270b536149c90bf0bf36a92a1541322300
GcsSloop <GcsSloop@gmail.com>
2017-03-27 02:31:35 +0800

    消除重复刷新

--- diycode-app/src/main/java/com/gcssloop/diycode/fragment/TopicListFragment.java
+++ diycode-app/src/main/java/com/gcssloop/diycode/fragment/TopicListFragment.java
@@ -105,6 +105,11 @@ public class TopicListFragment extends BaseFragment {
         initListener(holder);
     }
 
+    @Override
+    protected void onFirstTimeLaunched() {
+        loadData();
+    }
+
     // 加载数据，默认从缓存加载
     private void loadData() {
         List<Topic> topics = mDataCache.getTopicsList();
@@ -230,7 +235,6 @@ public class TopicListFragment extends BaseFragment {
     public void onStart() {
         super.onStart();
         EventBus.getDefault().register(this);
-        loadData();
     }
 
     @Override
