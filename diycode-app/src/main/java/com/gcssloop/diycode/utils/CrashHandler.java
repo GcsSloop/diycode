f9c1c6884613a28e5e9bba8e7d6b41599c483762
GcsSloop <GcsSloop@gmail.com>
2017-04-04 21:06:42 +0800

    移除垃圾代码

--- diycode-app/src/main/java/com/gcssloop/diycode/utils/CrashHandler.java
+++ diycode-app/src/main/java/com/gcssloop/diycode/utils/CrashHandler.java
@@ -43,33 +43,6 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
      */
     @Override
     public void uncaughtException(Thread arg0, Throwable arg1) {
-        /*
-        String logPath;
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            logPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + File.separator + "log";
-            File file = new File(logPath);
-            if (!file.exists()) {
-                file.mkdirs();
-            }
-            try {
-                FileWriter fw = new FileWriter(logPath + File.separator + "errorlog.log", true);
-                fw.write(new Date() + "\n");
-                // 错误信息
-                // 这里还可以加上当前的系统版本，机型型号 等等信息
-                StackTraceElement[] stackTrace = arg1.getStackTrace();
-                fw.write(arg1.getMessage() + "\n");
-                for (int i = 0; i < stackTrace.length; i++) {
-                    fw.write("file:" + stackTrace[i].getFileName() + " class:" + stackTrace[i].getClassName() + " method:" + stackTrace[i].getMethodName() + " line:" + stackTrace[i].getLineNumber() + "\n");
-                }
-                fw.write("\n");
-                fw.close();
-                // 上传错误信息到服务器
-                // uploadToServer();
-            } catch (IOException e) {
-                Log.e("crash handler", "load file failed...", e.getCause());
-            }
-        }
-        */
         arg1.printStackTrace();
         android.os.Process.killProcess(android.os.Process.myPid());
     }
